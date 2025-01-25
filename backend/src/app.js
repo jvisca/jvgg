e1044ffc46fe4e73bd89e8cab5d9f386dd3c304d
jvisca <jvisca@fi.uba.ar>
2025-01-24 21:48:31 -0300

    Express Skeleton

---
+++ backend/src/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/api/v1/')
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+      
