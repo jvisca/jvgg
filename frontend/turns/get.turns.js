d66feca407056943ae91f5d7f4e7881e84781371
Gaspar <ggutierrez@fi.uba.ar>
2025-02-06 16:56:09 -0300

    front package.json & navbar burguer

--- frontend/turns/get.turns.js
+++ frontend/turns/get.turns.js
@@ -89,3 +89,17 @@ deleteTurn = function(id){
         alert('Error deleting the turn: ' + error.message);
     });
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+    if ($navbarBurgers.length > 0) {
+        $navbarBurgers.forEach(el => {
+                el.addEventListener('click', () => {
+                    const target = el.dataset.target;
+                    const $target = document.getElementById(target);
+                    el.classList.toggle('is-active');
+                    $target.classList.toggle('is-active');
+            });
+        });
+    }
+});
--- frontend/users/get.user.js
+++ frontend/users/get.user.js
@@ -81,3 +81,16 @@ deleteUser = function(dni) {
         alert('Error deleting the user: ' + error.message);
     });
 }
+document.addEventListener('DOMContentLoaded', () => {
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+    if ($navbarBurgers.length > 0) {
+        $navbarBurgers.forEach(el => {
+                el.addEventListener('click', () => {
+                    const target = el.dataset.target;
+                    const $target = document.getElementById(target);
+                    el.classList.toggle('is-active');
+                    $target.classList.toggle('is-active');
+            });
+        });
+    }
+});
